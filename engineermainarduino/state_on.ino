996fb417003b579241463d6829fcabdfaabeb6c7
Lex Robinson <lexi@lexi.org.uk>
2014-11-29 19:11:45 +0000

    fix - if you don't set the LEDs every frame, ship damage blanks them.

--- engineermainarduino/state_on.ino
+++ engineermainarduino/state_on.ino
@@ -1,23 +1,21 @@
 // on
 
 void setStateOn() {
-  for (int i = 0; i < NUM_PANEL_LEDS; i++) {
-    ledsPanel[i] = BrightGreen;
-  }
-  // power light
-  *ledReactor = BrightGreen;
   // Power rings
   ringLightState = true;
 }
 
 // On state, set everything to green
 void stateOn() {
-  // for (int i = 0; i < NUM_PANEL_LEDS; i++) {
-  //   if (switches[i] != SWITCH_POS_UP) {
-  //     reset();
-  //     return
-  //   }
-  // }
+  for (int i = 0; i < NUM_PANEL_LEDS; i++) {
+    ledsPanel[i] = BrightGreen;
+    // if (switches[i] != SWITCH_POS_UP) {
+    //   reset();
+    //   return
+    // }
+  }
+  // power light
+  *ledReactor = BrightGreen;
 
   // if someone turns reactor switch off then
   if (switches[REACTOR_SWITCH] != REACTOR_SWITCH_ON) {
